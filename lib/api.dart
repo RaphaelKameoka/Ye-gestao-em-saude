f2961d3d80b06c02d6ebd8c43a329e8c4e1a7a6b
Leonardo Cardenuto <130413069+leonardocardenuto@users.noreply.github.com>
2024-05-03 09:30:04 -0300

    Update api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class ApiClient {
-  static const String _baseUrl = 'http://192.168.0.114:8000';
+  static const String _baseUrl = 'http://192.168.0.114:3000';
 
   Future<http.Response> get(String route) async {
     final response = await http.get(Uri.parse('$_baseUrl$route'));
