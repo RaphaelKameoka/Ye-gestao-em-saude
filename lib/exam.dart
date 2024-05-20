ddb5715ce21e7e398a2749ff28fb0a5ecb7a6c8d
Raphael Kameoka <raphaelhiro8@gmail.com>
2024-05-20 20:36:11 -0300

    Dropdown Exames

--- lib/exam.dart
+++ lib/exam.dart
@@ -24,57 +24,166 @@ class _ExamScreenState extends State<ExamScreen> {
     return Column(
       children: [
         SizedBox(height: 60),
-        SingleChildScrollView(
-          child: Container(
-            child: ExpansionPanelList(
-              expansionCallback: (int index, bool isExpanded) {
-                setState(() {
-                  _data[index].isExpanded = !isExpanded;
-                });
-              },
-              children: _data.map<ExpansionPanel>((Item item) {
-                return ExpansionPanel(
-                  headerBuilder: (BuildContext context, bool isExpanded) {
-                    return Column(
-                      children: [
-                        ListTile(
-                          title: Text(item.headerValue),
-                        ),
-                      ],
-                    );
-                  },
-                  body: Column(
-                    children: [
-                      ListTile(
-                        title: Text(item.pressaoValue),
-                      ),
-                      ListTile(
-                        title: Text(item.glicemiaValue),
-                      ),
-                      ListTile(
-                        title: Text(item.pesoValue),
-                      ),
-                      ListTile(
-                        title: Text(item.imcValue),
-                      ),
-                    ],
-                  ),
+        Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 241, 241, 234),
+          ),
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Container(
+              child: ExpansionPanelList(
+                expansionCallback: (int index, bool isExpanded) {
+                  setState(() {
+                    _data[index].isExpanded = !isExpanded;
+                  });
+                },
+                children: _data.map<ExpansionPanel>((Item item) {
+                  return ExpansionPanel(
+                    hasIcon: false,
+                    backgroundColor: Color.fromARGB(255, 241, 241, 234),
+                    headerBuilder: (BuildContext context, bool isExpanded) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: Text(
+                              item.headerValue,
+                              style: GoogleFonts.montserrat(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22),
+                            ),
+                            leading: Icon(
+                              isExpanded ? Icons.remove : Icons.add
+                            ),
+                            onTap: _expand,
+                          )
+                        ],
+                      );
+                    },
 
-                  isExpanded: item.isExpanded,
-                );
-              }).toList(),
+                    body: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 217, 217, 217),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  title: Text(item.pressaoValue,
+                                      style: GoogleFonts.montserrat(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20)),
+                                ),
+                              ),
+                              Text("12/8",
+                                  style: GoogleFonts.montserrat(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                              SizedBox(width: 20)
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 217, 217, 217),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  title: Text(item.glicemiaValue,
+                                      style: GoogleFonts.montserrat(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20)),
+                                ),
+                              ),
+                              Text("70 mg/dl",
+                                  style: GoogleFonts.montserrat(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                              SizedBox(width: 20)
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 217, 217, 217),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  title: Text(item.pesoValue,
+                                      style: GoogleFonts.montserrat(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20)),
+                                ),
+                              ),
+                              Text("75 kg",
+                                  style: GoogleFonts.montserrat(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                              SizedBox(width: 20)
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 217, 217, 217),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  title: Text(item.imcValue,
+                                      style: GoogleFonts.montserrat(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20)),
+                                ),
+                              ),
+                              Text("20.0",
+                                  style: GoogleFonts.montserrat(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                              SizedBox(width: 20)
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 30,)
+                      ],
+                    ),
+                    isExpanded: item.isExpanded,
+                  );
+                }).toList(),
+              ),
             ),
           ),
         ),
-        TextButton(
-            onPressed: () {
-              _expand();
-            },
-            child: Text("Expandir"))
       ],
     );
   }
-
 }
 
 class Item {
@@ -84,7 +193,7 @@ class Item {
     required this.imcValue,
     required this.pressaoValue,
     required this.headerValue,
-    this.isExpanded = true,
+    this.isExpanded = false,
   });
 
   String glicemiaValue;
@@ -99,10 +208,9 @@ List<Item> generateItems(int numberOfItems) {
   return List<Item>.generate(numberOfItems, (int index) {
     return Item(
         headerValue: 'Exames mais recentes',
-        pressaoValue: 'pressao',
-        glicemiaValue: "glicemia",
+        pressaoValue: 'Pressão',
+        glicemiaValue: "Glicemia",
         pesoValue: 'Peso',
-        imcValue: "imc");
+        imcValue: "IMC");
   });
-
 }
