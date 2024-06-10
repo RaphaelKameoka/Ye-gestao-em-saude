d687f1411802e6f91579f969bea7d4edfb26f002
Raphael Kameoka <raphaelhiro8@gmail.com>
2024-06-09 22:33:48 -0300

    home-background bottomNavigationBar fixed

--- lib/background/home_background.dart
+++ lib/background/home_background.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ye_project/exam.dart';
 import 'package:ye_project/chat.dart';
 import 'package:ye_project/history.dart';
 import 'package:ye_project/medication.dart';
 
-class HomeBackground extends StatefulWidget {
-  HomeBackground(this.nextScreen, {Key? key}) : super(key: key);
 
+class HomeBackground extends StatefulWidget {
   final String nextScreen;
 
+  HomeBackground(this.nextScreen, {Key? key}) : super(key: key);
+
   @override
-  State<HomeBackground> createState() {
-    return _HomeBackgroundState();
-  }
+  State<HomeBackground> createState() => _HomeBackgroundState();
 }
 
 class _HomeBackgroundState extends State<HomeBackground> {
-  Color profileCor = Colors.transparent;
-  Color examCor = Colors.grey[350] ?? Colors.grey;
-  Color notificationsCor = Colors.transparent;
-
   late String userName;
   late String avatar;
+  int _selectedIndex = 1;
+  late PageController _pageController;
 
   @override
   void initState() {
     super.initState();
     userName = '';
     avatar = '';
+    _pageController =
+        PageController(initialPage: 1);
   }
 
   @override
@@ -58,24 +58,15 @@ class _HomeBackgroundState extends State<HomeBackground> {
     });
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget actualHomeScreen = ExamScreen(userName: userName, avatar: avatar);
-
-    switch (widget.nextScreen) {
-      case 'chat_with_ai':
-        actualHomeScreen = ChatScreen();
-        break;
-      case 'exam':
-        actualHomeScreen = ExamScreen(userName: userName, avatar: avatar,);
-        break;
-      case 'history':
-        actualHomeScreen = HistoryScreen(userName: userName);
-        break;
-      case 'medication':
-        actualHomeScreen = MedicationScreen(userName: userName);
-    }
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Stack(
@@ -85,181 +76,67 @@ class _HomeBackgroundState extends State<HomeBackground> {
               color: const Color(0xFF6B9683),
             ),
           ),
-          Positioned.fill(child: actualHomeScreen),
-          if (widget.nextScreen != "chat_with_ai")
-            Positioned(
-              bottom: 60,
-              right: 20,
-              child: GestureDetector(
-                onTap: () {
-                  _navigateTo('/chat_with_ai');
-                },
-                child: Container(
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color.fromARGB(255, 46, 100, 180),
-                  ),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/png/ai-stars.png',
-                      width: 60,
-                      height: 70,
-                    ),
+          PageView(
+            controller: _pageController,
+            onPageChanged: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            children: [
+              HistoryScreen(userName: userName),
+              ExamScreen(userName: userName, avatar: avatar),
+              MedicationScreen(userName: userName),
+            ],
+          ),
+          Positioned(
+            bottom: 60,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                _navigateTo('/chat_with_ai');
+              },
+              child: Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromARGB(255, 46, 100, 180),
+                ),
+                child: Center(
+                  child: Image.asset(
+                    'assets/png/ai-stars.png',
+                    width: 60,
+                    height: 70,
                   ),
                 ),
               ),
             ),
+          ),
         ],
       ),
-      bottomNavigationBar: Container(
-        color: const Color.fromARGB(255, 241, 241, 234),
-        height: 80,
-        width: double.infinity,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedContainer(
-                  duration: const Duration(milliseconds: 300),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                  decoration: BoxDecoration(
-                    color: profileCor,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        profileCor = Colors.grey[350] ?? Colors.grey;
-                        examCor = Colors.transparent;
-                        notificationsCor = Colors.transparent;
-                        Future.delayed(const Duration(milliseconds: 50), () {
-                          _navigateTo('/history');
-                        });
-                      });
-                    },
-                    icon: Column(
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/png/history.png',
-                          width: 70,
-                          height: 40,
-                        ),
-                        Text(
-                          "Histórico",
-                          style: GoogleFonts.montserrat(
-                            color: const Color(0xFF6B9683),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    highlightColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    padding: EdgeInsets.zero,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(width: 17),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedContainer(
-                  duration: const Duration(milliseconds: 300),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                  decoration: BoxDecoration(
-                    color: examCor,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        examCor = Colors.grey[350] ?? Colors.grey;
-                        profileCor = Colors.transparent;
-                        notificationsCor = Colors.transparent;
-                      });
-                      Future.delayed(const Duration(milliseconds: 50), () {
-                        _navigateTo('/exam');
-                      });
-                    },
-                    icon: Column(
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/png/medical_exam.png',
-                          width: 70,
-                          height: 40,
-                        ),
-                        Text(
-                          "Exames",
-                          style: GoogleFonts.montserrat(
-                            color: const Color(0xFF6B9683),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    highlightColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    padding: EdgeInsets.zero,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(width: 17),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedContainer(
-                  duration: const Duration(milliseconds: 300),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-                  decoration: BoxDecoration(
-                    color: notificationsCor,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        notificationsCor = Colors.grey[350] ?? Colors.grey;
-                        profileCor = Colors.transparent;
-                        examCor = Colors.transparent;Future.delayed(const Duration(milliseconds: 50), () {
-                          _navigateTo('/medication');
-                        });
-                      });
-                    },
-                    icon: Column(
-                      children: <Widget>[
-                        Image.asset(
-                          'assets/png/medication.png',
-                          width: 70,
-                          height: 40,
-                        ),
-                        Text(
-                          "Medicação",
-                          style: GoogleFonts.montserrat(
-                            color: const Color(0xFF6B9683),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    highlightColor: Colors.transparent,
-                    splashColor: Colors.transparent,
-                    padding: EdgeInsets.zero,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 241, 241, 234),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.clockRotateLeft, size: 30),
+            label: 'Histórico',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.briefcaseMedical, size: 30,),
+            label: 'Exames',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.pills, size: 30),
+            label: 'Medicação',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xFF6B9683),
+        selectedLabelStyle: const TextStyle(color: Color(0xFF6B9683)),
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+
